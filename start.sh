be6aab443bb59de3733b65160bf98ed191646429
el pepe <gen.elpepes@gmail.com>
2025-03-07 21:58:16 +0000

    Refactor application configuration and update Lavalink setup

--- start.sh
+++ start.sh
@@ -1,3 +1,14 @@
-curl "https://ci.fredboat.com/repository/download/Lavalink_Build/.lastSuccessful/Lavalink.jar?guest=1&branch=refs/heads/dev" -o Lavalink.jar
+URL="https://github.com/lavalink-devs/Lavalink/releases/download/4.0.8/Lavalink.jar"
+OUTPUT="Lavalink.jar"
 
-java -jar Lavalink.jar
+# Download the Lavalink JAR file
+curl -fSL "$URL" -o "$OUTPUT"
+
+# Check if the download was successful
+if [ $? -eq 0 ]; then
+    echo "Download successful, starting Lavalink..."
+    java -jar "$OUTPUT"
+else
+    echo "Download failed, please check the URL or your internet connection."
+    exit 1
+fi
